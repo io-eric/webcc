fc23e0309fc1a296f6b790a078cd092e0286723d
io-eric <nicknickelseb@gmail.com>
2025-12-15 05:11:24 +0100

    bugfix: correcting output path and source file locations

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@
 set -e
 
 echo "[WebCC] Building webcc tool..."
-g++ -std=c++17 -O2 -o webcc/webcc \
-    webcc/cli/webcc.cc \
-    webcc/src/webcc.cc \
-    webcc/src/command_buffer.cc \
-    -I webcc/include
+g++ -std=c++17 -O2 -o webcc \
+    src/webcc.cc \
+    src/command_buffer.cc \
+    -I include
 
-echo "[WebCC] Build complete: webcc/webcc"
+echo "[WebCC] Build complete: webcc"
