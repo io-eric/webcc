a1d78fc0e4f4ebc5a0e221adcb382a0f31a65130
io-eric <nicknickelseb@gmail.com>
2026-01-07 19:06:26 +0100

    feat: Add meta tags for character set and viewport in generated HTML

--- src/cli/generators.cc
+++ src/cli/generators.cc
@@ -937,6 +937,10 @@ namespace webcc {
         std::string html = R"(
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+</head>
 <body>
     <script src="app.js"></script>
 </body>
